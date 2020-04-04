0d704b6319660fdea6785548b415f0c797c844e3
safy97 <mostafasafy1997@gmail.com>
2020-04-04 17:50:24 +0200

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :stock, presence: true
+  validate :has_one_category_at_least
+
+  def has_one_category_at_least
+    if categories.empty?
+      errors.add(:categories, "Product should at least has one category")
+    end
+  end
 end
