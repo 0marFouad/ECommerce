bc38d81823567b5dc65912d2793b513fb7f06a65
safy97 <mostafasafy1997@gmail.com>
2020-04-26 22:54:57 +0200

    create review

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user ,validate: {presence => true}
   has_and_belongs_to_many :categories
-
+  has_many :reviews
   validates :name, presence: true
   validates :price, presence: true
   validates :stock, presence: true
