2ea1417431e1d3840f328c625eff4fe0cf064163
safy97 <mostafasafy1997@gmail.com>
2020-04-27 00:09:34 +0200

    add orders and link it with products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user ,validate: {presence => true}
   has_and_belongs_to_many :categories
   has_many :reviews
+  has_many :order_products
   validates :name, presence: true
   validates :price, presence: true
   validates :stock, presence: true
