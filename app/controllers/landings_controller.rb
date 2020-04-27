37215da5432a22eeb164f1495322c6a90a9fa094
safy97 <mostafasafy1997@gmail.com>
2020-04-27 03:42:23 +0200

    add search functionality

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,4 +2,12 @@ class LandingsController < ApplicationController
   def index
 
   end
+  def search
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
+      @parameter = params[:search].downcase
+      @results = Product.all.where("lower(name) LIKE  ? ", @parameter+'%')
+    end
+  end
 end
