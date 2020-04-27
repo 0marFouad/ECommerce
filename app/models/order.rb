9ad1c51fd90fe0ec7c0711e17bd3b6c70bec981f
safy97 <mostafasafy1997@gmail.com>
2020-04-27 04:01:40 +0200

    fix status validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,5 @@ class Order < ApplicationRecord
 
   validates :shipping_date, :presence => true
   validates :shipping_address , :presence => true
-  validates :status , :presence => true
+  validates :status , inclusion: { in: [ true, false ] }, allow_nil: false
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,4 +4,12 @@ class ReviewTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def get_user
+    user = User.create(email: 'user11@gmail.com', password: '123456',name: 'user' , telephone: '0123456789')
+  end
+
+  test "create review without user" do
+    review = Review.new()
+  end
+
 end
