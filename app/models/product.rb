9fe9eab68b6f42c78a88675934aa008f86a47d32
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-05-10 08:49:24 +0200

    Added Shopping Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
+  before_destroy :not_referenced_by_any_cart_item
   belongs_to :user ,validate: {presence => true}
   has_and_belongs_to_many :categories
   has_many :reviews
+  has_many :cart_items
   has_many :order_products
   validates :name, presence: true
   validates :price, presence: true
@@ -11,6 +13,15 @@ class Product < ApplicationRecord
   def has_one_category_at_least
     if categories.empty?
       errors.add(:categories, "Product should at least has one category")
+    end
+  end
+
+  private
+
+  def not_referenced_by_any_cart_item
+    unless cart_items.empty?
+      errors.add(:base, 'Cart items present')
+      throw :abort
     end
   end
 end
