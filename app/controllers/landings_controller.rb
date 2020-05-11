84110b9a8d680e49138b0d4c50847c88ddf1ad33
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-05-11 09:36:28 +0200

    Backend Done

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -10,4 +10,21 @@ class LandingsController < ApplicationController
       @results = Product.all.where("lower(name) LIKE  ? ", @parameter+'%')
     end
   end
+
+  #7andeef
+  def custom_search
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
+      @search_word = params[:search].downcase
+      @results = Category.where(id: params[:category_id][:category_id]).first.products
+      @results = @results.all.where("lower(name) LIKE  ? ", @search_word+'%')
+      @results = @results.where("price >= ?", params[:min_price])
+      @results = @results.where("price <= ?", params[:max_price])
+    end
+  end
+
+  def view_search
+    render :view_search
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landings#index'
   get '/search' => 'landings#search', :as => 'search_page'
+  get '/custom_search' => 'landings#custom_search', :as => 'custom_search_page'
+  get '/custom_search_query' => 'landings#view_search', :as => 'custom_search_query'
   post 'product/:id/rate' => 'products#rate', :as => 'rate_product'
 end
